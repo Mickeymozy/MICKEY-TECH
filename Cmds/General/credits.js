67283a2788046d604969b9a642a16700b2790a65
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-07-05 22:45:41 +0300

    Update credits.js

--- Cmds/General/credits.js
+++ Cmds/General/credits.js
@@ -9,7 +9,7 @@ module.exports = async (context) => {
     const { client, m, prefix } = context;
 
 
-           await client.sendMessage(m.chat, { image: { url: 'https://telegra.ph/file/c75efecf7f0aef851fc02.jpg' }, caption: `We express sincere gratitude and acknowledgement to the following:\n\n -Dika Ardnt ➪ Indonesia\n - Writing the base code using case method\nhttps://github.com/DikaArdnt\n\n -Adiwajshing ➪ India\n - Writing and Coding the bot's library (baileys)\nhttps://github.com/WhiskeySockets/Baileys\n\n -WAWebSockets Discord Server community\n-Maintaining and reverse engineering the Web Sockets\nhttps://discord.gg/WeJM5FP9GG\n\n - Fortunatus Mokaya ➪ Kenya\n - Actively compiling and debugging parts of this bot script\nhttps://github.com/Fortunatusmokaya\n - Malik ➪ Kenya\n - Actively de-compiling, debugging and fixing parts of this bot script\nhttps://github.com/darkLo1rd\n\n - ChatGPT ➪ USA\n - Formulating ideas and assisting in debugging.\nhttps://chat.openai.com\n\n𝐷𝑟𝑒𝑎𝑑𝑒𝑑 𝐵𝑜𝑡 シ︎`}); 
+           await client.sendMessage(m.chat, { image: { url: 'https://files.catbox.moe/ljabyq.png' }, caption: `To any payment services those is payment methods:\n\n -Halotel ➪ Tanzania\n - number 0615944741 name; MICKDADI HAMZA\n\n -Yas ➪ AfricaAa\n - number 0711765335 name; MOSSI MATUMLA \n\n\n- NMB ➪ BANK\n - For bank user waiting until develop card.\n\n{mickey-tech} シ︎`}); 
 
 
 }
