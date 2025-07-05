a4af4f2090fe7f83475a86214bf15e927f3f9be3
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-07-05 22:35:07 +0300

    Update alive.js

--- Cmds/General/alive.js
+++ Cmds/General/alive.js
@@ -5,6 +5,6 @@ module.exports = async (context) => {
 
 const botname = process.env.BOTNAME || "MICKEY-TECH";
 
- await client.sendMessage(m.chat, { image: { url: 'https://ibb.co/L275xWK' }, caption: `Hello ${m.pushName}, Mickey-tech is active now.\n\nType ${prefix}menu to see my command list..\n\nDont forget to fork and star my repo \.\n\nXd );`, fileLength: "9999999999898989899999999" }, { quoted: m }); 
+ await client.sendMessage(m.chat, { image: { url: 'https://files.catbox.moe/5n6dz2.jpg' }, caption: `Hello ${m.pushName}, Mickey-tech is active now.\n\nType ${prefix}menu to see my command list..\n\nDont forget to fork and star my repo \.\n\nXd );`, fileLength: "9999999999898989899999999" }, { quoted: m }); 
 
 }
